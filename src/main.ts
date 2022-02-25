f70d46b58aa2e7218d2248485d6c8fb6a2be7d06
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-02-25 19:45:41 +0100

    no csrf bc pain in the ass

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import { AppModule } from './app.module';
 import * as csurf from 'csurf';
 import * as fs from 'fs';
+import session from 'express-session';
 
 const httpsOptions = {
   key: fs.readFileSync('./secrets/private-key.pem'),
@@ -13,8 +14,15 @@ const httpsOptions = {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { httpsOptions });
   app.use(helmet());
-  app.use(cookieParser());
-  app.use(csurf());
+  // app.use(cookieParser());
+  // app.use(
+  //   session({
+  //     secret: 'your-secret',
+  //     resave: false,
+  //     saveUninitialized: false,
+  //   })
+  // );
+  // app.use(csurf());
   await app.listen(3000);
 }
 bootstrap();
