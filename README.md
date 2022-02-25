da30a86f11cce696623e98adea2fa693457254fb
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-02-25 18:52:25 +0100

    https

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # Backend for the Website of Christoph Anton-Cornelius Bärtsch
+
 ## Setup
+
 - fill the `ormconfig.json` file with your database connection
+- replace the certificates in `/secrets` with your own
 - run `typeorm schema:sync` once to sync the database with the schema
+
 ## API Documentation
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import * as csurf from 'csurf';
+import * as fs from 'fs';
+
+const httpsOptions = {
+  key: fs.readFileSync('./secrets/private-key.pem'),
+  cert: fs.readFileSync('./secrets/public-certificate.pem'),
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.use(helmet());
   app.use(cookieParser());
   app.use(csurf());
