8dd8eb368a90592bbee7752b8ec83f3186576b1d
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2022-03-08 21:40:40 +0100

    sum exception handling

--- src/videos/videos.service.ts
+++ src/videos/videos.service.ts
@@ -6,6 +6,9 @@ import { Videos } from './videos.entity';
 import { MediaService } from '../media/media.service';
 import { Constants } from '../constants';
 import { randomUUID } from 'crypto';
+import * as fs from 'fs';
+import path from 'path';
+import { join } from 'path';
 
 export interface VideoBody {
   url: string;
@@ -38,32 +41,45 @@ export class VideosService {
     video: Express.Multer.File,
     images: Express.Multer.File[]
   ): Promise<Videos> {
-    try {
-      const videoUUID = randomUUID();
-      if (!(await this.mediaService.waterMarkVideo(video.path, Constants.videos_path, videoUUID))) {
-        throw new InternalServerErrorException('Could not watermark video');
-      }
-      // TODO: add watermark to images
-      for await (const image of images) {
-        const imageUUID = randomUUID();
-        if (
-          !(await this.mediaService.waterMarkImage(
-            image.path,
-            Constants.videos_images_path,
-            imageUUID
-          ))
-        ) {
-          throw new InternalServerErrorException('Could not watermark image');
-        }
-      }
-      // TODO: save video -> done in watermarkVideo
-      // TODO: save images -> done in watermarkImage
-      // TODO: save metadata
-    } catch (error) {
-      // TODO: delete video, images and metadata
-      console.error(error);
-      throw new InternalServerErrorException('Could not create video');
+    const videoUUID = randomUUID();
+    const imageUUIDS: string[] = [];
+    if (!(await this.mediaService.waterMarkVideo(video.path, Constants.videos_path, videoUUID))) {
+      this.videoErrorCleanup(videoUUID, imageUUIDS);
+      throw new InternalServerErrorException('Could not watermark video');
     }
+    // TODO: add watermark to images
+    for await (const image of images) {
+      const imageUUID = randomUUID();
+      imageUUIDS.push(imageUUID);
+      if (
+        !(await this.mediaService.waterMarkImage(
+          image.path,
+          Constants.videos_images_path,
+          imageUUID
+        ))
+      ) {
+        this.videoErrorCleanup(videoUUID, imageUUIDS);
+        throw new InternalServerErrorException('Could not watermark image');
+      }
+    }
+    // TODO: save video -> done in watermarkVideo
+    // TODO: save images -> done in watermarkImage
+    // TODO: save metadata
+    // TODO: delete video, images and metadata
     return null;
+  }
+
+  private videoErrorCleanup(videoUUID: string, imageUUIDS: string[]) {
+    fs.rmSync(join(Constants.videos_path, videoUUID + Constants.video_extension), {
+      force: true,
+      recursive: true,
+    });
+
+    for (const imageUUID of imageUUIDS) {
+      fs.rmSync(join(Constants.videos_images_path, imageUUID + Constants.image_extension), {
+        force: true,
+        recursive: true,
+      });
+    }
   }
 }
