589f27a4e28465ceeefa729b0319c262e3fcc0dc
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-03-12 19:11:03 +0100

    song post

--- src/music/music/music.controller.ts
+++ src/music/music/music.controller.ts
@@ -1,4 +1,76 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  ConflictException,
+  Controller,
+  Get,
+  Post,
+  UploadedFiles,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Music } from '../music.entity';
+import { MusicService } from './music.service';
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { Constants } from '../../constants';
 
 @Controller('music')
-export class MusicController {}
+export class MusicController {
+  constructor(private readonly musicService: MusicService) {}
+
+  @Get()
+  getAll() {
+    return this.musicService.getAll();
+  }
+
+  @Post()
+  @UseGuards(JwtAuthGuard)
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: 'song', maxCount: 1 },
+        { name: 'cover', maxCount: 1 },
+      ],
+      {
+        dest: Constants.temp_upload_path,
+        fileFilter: (req, file, callback) => {
+          switch (file.fieldname) {
+            case 'song':
+              if (file.mimetype !== 'audio/mpeg') {
+                callback(new BadRequestException('Invalid song file'), false);
+              } else {
+                callback(null, true);
+              }
+              break;
+            case 'cover':
+              if (file.mimetype !== 'image/jpeg') {
+                callback(new BadRequestException('Invalid cover file'), false);
+              } else {
+                callback(null, true);
+              }
+              break;
+            default:
+              break;
+          }
+        },
+      }
+    )
+  )
+  async create(
+    @UploadedFiles() files: { song?: Express.Multer.File[]; cover?: Express.Multer.File[] },
+    @Body() url: { url: string }
+  ): Promise<Music> {
+    if (files.song == undefined || files.cover == undefined) {
+      throw new BadRequestException('Invalid files');
+    }
+    if (await this.musicService.checkIfSongExists(files.song[0].path)) {
+      throw new ConflictException('Song already exists');
+    }
+    return this.musicService.create({
+      url: url.url,
+      song: files.song[0],
+      image: files.cover[0],
+    });
+  }
+}
--- src/music/music/music.service.ts
+++ src/music/music/music.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MediaService } from 'src/media/media.service';
 import { Repository } from 'typeorm';
 import { Music } from '../music.entity';
+import { randomUUID } from 'crypto';
+import * as fs from 'fs';
+import { Constants } from '../../constants';
 
 @Injectable()
 export class MusicService {
@@ -17,5 +20,46 @@ export class MusicService {
 
   getAll() {
     return this.mediaService.getAll(this.stillsRepository);
+  }
+
+  async create(musicPost: { url: string; image: Express.Multer.File; song: Express.Multer.File }) {
+    const music = new Music();
+    music.url = musicPost.url;
+    music.hash = await this.mediaService.hashFile(musicPost.song.path);
+    music.id = randomUUID();
+    music.pictureId = randomUUID();
+
+    fs.mkdirSync(Constants.music_path, { recursive: true });
+    fs.mkdirSync(Constants.music_images_path, { recursive: true });
+    fs.copyFileSync(
+      musicPost.song.path,
+      Constants.music_path + '/' + music.id + Constants.music_extension
+    );
+    fs.copyFileSync(
+      musicPost.image.path,
+      Constants.music_images_path + '/' + music.pictureId + Constants.image_extension
+    );
+    fs.rmSync(musicPost.song.path, { recursive: true, force: true });
+    fs.rmSync(musicPost.image.path, { recursive: true, force: true });
+
+    let count = 0;
+    while (count < 10) {
+      count++;
+      music.position = await this.stillsRepository.count();
+      try {
+        const save = await this.stillsRepository.save(music);
+        return save;
+      } catch (error) {
+        continue;
+      }
+    }
+
+    throw new InternalServerErrorException('Could not save music');
+  }
+
+  async checkIfSongExists(songPath: string) {
+    const hash = await this.mediaService.hashFile(songPath);
+    const result = await this.stillsRepository.findOne({ where: { hash: hash } });
+    return result != undefined;
   }
 }
