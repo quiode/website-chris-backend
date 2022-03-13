ee02a9c52171dffc7f27a41b0cfe0f14c05455f5
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-03-13 12:53:57 +0100

    no https

--- src/main.ts
+++ src/main.ts
@@ -15,7 +15,7 @@ const httpsOptions = {
 };
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, { httpsOptions });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'public'), { index: false, prefix: '/public/' });
   app.use(helmet());
   // app.use(cookieParser());
