7727dda00e5ab6dc32ab9f89baaa48ddbc18732d
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-03-13 14:24:19 +0100

    idk anymore

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
           styleSrc: [`'self'`, `'unsafe-inline'`, 'cdn.jsdelivr.net', 'fonts.googleapis.com'],
           fontSrc: [`'self'`, 'fonts.gstatic.com'],
           imgSrc: [`'self'`, 'data:', 'cdn.jsdelivr.net'],
-          scriptSrc: [`'self'`, `https: 'unsafe-inline'`, `cdn.jsdelivr.net`],
+          scriptSrc: [`'self'`, `https: 'unsafe-inline'`, `cdn.jsdelivr.net`, 'unsafe-hashes'],
         },
       },
     })
