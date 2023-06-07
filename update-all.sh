9d9c6ba4231b544fae5356853eda8179fd93733c
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2023-06-07 22:48:04 +0200

    remove unused test/deployment files

--- update-all.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cd ../Website-Chris/ && sudo rm -r dist && git pull && ng build && sudo rm -r ../website-chris-backend/website/* && sudo cp -r dist/website-chris/* ../website-chris-backend/website/ && cd ../website-chris-backend/ && sudo systemctl stop chris.service && git pull && sudo rm -r dist && npm run build && sudo systemctl start chris.service
